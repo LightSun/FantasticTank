3d97c259cc1de88bb21761c31b62eca965d2f84c
LinghtSun <532278976@qq.com>
2014-12-02 20:18:05 +0800

    no

--- android/src/com/heaven7/angrytank/android/AndroidLauncher.java
+++ android/src/com/heaven7/angrytank/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.heaven7.angrytank.MyGdxGame;
+import com.heaven7.fantastictank.FantasticTank;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		initialize(new FantasticTank(), config);
 	}
 }
